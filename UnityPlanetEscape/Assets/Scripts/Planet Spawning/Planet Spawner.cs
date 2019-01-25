1854be5b4216fb1037303ed27ba68788fdfa9c10
Unvios22 <unvios22@gmail.com>
2019-01-25 21:41:59 +0100

    WIP planet spawning

---
+++ UnityPlanetEscape/Assets/Scripts/Planet Spawning/Planet Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetSpawner : MonoBehaviour {
+	[SerializeField] private float spawnStartYValue;
+	[SerializeField] private float spawnStopYValue;
+	[SerializeField] private float spawnInteval;
+
+	private void Start() {
+		
+	}
+
+	private IEnumerator SpawnPlanets() {
+		var counter = 0f;
+		for (;;) {
+			if (counter >= spawnInteval) {
+				SpawnPlanet();
+				counter = 0;
+			}
+			counter += Time.deltaTime;
+		}
+		yield return 
+	}
+
+	private void SpawnPlanet() {
+		
+	}
+}
