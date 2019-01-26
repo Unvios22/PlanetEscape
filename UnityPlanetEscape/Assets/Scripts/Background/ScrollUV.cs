f153502738f02c63aade42b51101e9f18579ef8d
Kuba Prusakiewicz <jakubprusakiewicz@gmail.com>
2019-01-26 06:35:49 +0100

    feat nice background

---
+++ UnityPlanetEscape/Assets/Scripts/Background/ScrollUV.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollUV : MonoBehaviour
+
+{
+    private MeshRenderer mr;
+    private Vector2 offset;
+    private Material mat;
+    void Start()
+    {
+        mr = GetComponent<MeshRenderer>();
+        mat = mr.material;
+        offset = mat.mainTextureOffset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset.x += Time.deltaTime / 10f;
+        mat.mainTextureOffset = offset;
+    }
+}
