88875af2afbca87b8f7fa65e840dfcd5ea74b980
Mikolaj <twardowskimikolaj@interia.pl>
2019-01-26 02:31:46 +0100

    GRAWITACJA
    4 raz wysyłam bo zapomniałem wiadomości napisać (+ poprawienie linijki w kodzie (nie potrzebne wypisywanie wiadomości))

---
+++ UnityPlanetEscape/Assets/Scripts/Gravity.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    Rigidbody2D rig;
+
+    public static List<Gravity> Attractors;
+
+    const float G = 6.674f;
+
+    private void FixedUpdate()
+    {
+
+        foreach(Gravity attractor in Attractors)
+        {
+            if (attractor != this)
+                Attract(attractor);
+        }
+
+    }
+
+    private void OnEnable()
+    {
+        if (Attractors == null)
+            Attractors = new List<Gravity>();
+        Attractors.Add(this);
+    }
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    void Attract(Gravity other)
+    {
+        Rigidbody2D rbOther = other.GetComponent<Rigidbody2D>();
+        Vector3 direction = rig.position - rbOther.position;
+
+        float distance = direction.magnitude;
+
+        if (distance == 0)
+            return;
+
+        float forceMagnitude = G * (rig.mass * rbOther.mass) / Mathf.Pow(distance, 2);
+        Vector3 force = direction.normalized * forceMagnitude;
+
+        rbOther.AddForce(force);
+    }
+
+
+
+}
