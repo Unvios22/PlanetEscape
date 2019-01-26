4e0352e048130ddf316b94f9e2541a04e215683b
Kuba Prusakiewicz <jakubprusakiewicz@gmail.com>
2019-01-26 13:20:19 +0100

    UI with sprites, and simple amount script

---
+++ UnityPlanetEscape/Assets/UIScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UIScript : MonoBehaviour
+{
+    public GameObject currentHPObj,maxHPObj;
+    Text currentHP,maxHP;
+    void Start()
+    {
+        currentHP = currentHPObj.GetComponent<Text>();
+        maxHP = maxHPObj.GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        currentHP.text = "abcd /";
+        maxHP.text = "yyyy";
+    }
+}
