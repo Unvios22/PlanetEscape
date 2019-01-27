6ad0f07dd77cea9702655ab1a077e5368f76bb4f
Kajetan Jóźwiak <unvios22@gmail.com>
2019-01-27 12:42:31 +0100

    resolving problem kurwa

--- UnityPlanetEscape/Assets/Scripts/Shooting/ShootingLaserFromPlanet.cs
+++ UnityPlanetEscape/Assets/Scripts/Shooting/ShootingLaserFromPlanet.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Security.Cryptography.X509Certificates;
+﻿using Shooting;
 using UnityEngine;
 
 public class ShootingLaserFromPlanet : MonoBehaviour {
