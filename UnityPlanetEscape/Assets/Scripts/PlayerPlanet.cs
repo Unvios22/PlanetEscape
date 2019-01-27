0b40e29aeb7c83b8a4f6f3492daa0e3157ea78d1
Kajetan Jóźwiak <unvios22@gmail.com>
2019-01-27 09:58:50 +0100

    tweaking planet damage values

--- UnityPlanetEscape/Assets/Scripts/PlayerPlanet.cs
+++ UnityPlanetEscape/Assets/Scripts/PlayerPlanet.cs
@@ -9,19 +9,13 @@ public class PlayerPlanet : MonoBehaviour {
 	//todo attach to player planet gameobject
 
 	public GameController gameController;
-
-	private void OnTriggerEnter(Collider other) {
-		if (other.gameObject.CompareTag(Tags.ASTEROID)) {
-			//todo
-		}
-	}
 	
 	private void OnTriggerEnter2D(Collider2D other) {
 		if (other.CompareTag(Tags.ASTEROID)) {
 			other.GetComponent<Asteroid>().DestroyAsteroid();
-			//todo minus population, and resources
-			gameController.population -= Random.Range(5f, 30f);
-			gameController.resources -= Random.Range(5f, 30f);
+			//minus pop and resources on collision with asteroid
+			gameController.population -= Random.Range(10f, 40f);
+			gameController.resources -= Random.Range(10f, 40f);
 		}
 	}
 }
