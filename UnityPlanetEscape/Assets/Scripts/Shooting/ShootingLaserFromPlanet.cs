949757d60f6725ae347e686bd6bb70983933fdb7
ZROS <zros98@gmail.com>
2019-01-27 02:08:03 +0100

    Animations

--- UnityPlanetEscape/Assets/Scripts/Shooting/ShootingLaserFromPlanet.cs
+++ UnityPlanetEscape/Assets/Scripts/Shooting/ShootingLaserFromPlanet.cs
@@ -4,93 +4,86 @@ using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class ShootingLaserFromPlanet : MonoBehaviour {
+    
     public GameObject laserBullet;
-
-    private int laserBulletCount;
+    public float planetRadius;
+    public Transform targetPlanet;
+    public int laserBulletCount;
     public int maxLaserBulletCount = 5;
 
     public int forcePower = 100;
-//    [SerializeField]GameObject target;
-//    [SerializeField] GameObject pocisk;
-//    Vector3 targetStartPosition;
-//    Vector3 direction;
-//    [SerializeField]float minDistance;
-//    [SerializeField]float maxDistance;
-//    float distance;
-//    Camera cam;
-//    Vector3 mousePosition;
-//    Vector3 temp;
 
-//
+    public float cameraHeight;
+
+    public float cameraWidth;
+
+    [SerializeField] GameObject planet;
+
+    float maxDistanceFrom;
+
+    Camera cam;
+
+    Vector3 mousePosition;
+    Vector3 temp;
+    private float distance;
+
     private void Start() {
-        Camera cam = Camera.main;
-        float height = 2f * cam.orthographicSize;
-        float width = height * cam.aspect;
+        
+        //Vector2 origin = planet.gameObject.transform.position;
+        //Vector2 onPlanet = Random.insideUnitCircle * planetRadius;
+        //GameObject newGo =Instantiate(gameObject,onPlanet,Quaternion.identity) as GameObject;
+        
+       // newGo.transform.rotation=newGo.transform.rotation*Quaternion.Euler(90, 0, 0);
+
+       
         
         
-        //laserBulletRb.GetComponent<Rigidbody2D>();
-//        cam = Camera.main;
-//        targetStartPosition = target.transform.position;
+        cam = Camera.main;
+        cameraHeight = cam.orthographicSize;
+        cameraWidth = cameraHeight * cam.aspect;
+
+
+        maxDistanceFrom = planet.GetComponent<CircleCollider2D>().radius;
+
     }
 
-//
     private void Update() {
+
+        //transform.LookAt(planet.transform.position.normalized);
+        transform.LookAt(new Vector3(0,0,0));
+        
+        
+        
+        
+        
+        mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+        distance = Vector3.Distance(transform.position, mousePosition);
+
+        if (mousePosition.x == 0)
+            mousePosition.x = 0.00001f;
+        float x = Mathf.Sqrt((maxDistanceFrom * maxDistanceFrom) / (1 + (mousePosition.y / mousePosition.x) * (mousePosition.y / mousePosition.x)));
+        float y = x * (mousePosition.y / mousePosition.x);
+        temp = new Vector3(x, y, 0) + planet.transform.position;
+        if (Vector3.Distance(mousePosition, temp) > Vector3.Distance(mousePosition, -temp))
+            transform.position = (-temp);
+        else
+            transform.position = temp;
+
         if (Input.GetKeyDown("space") && laserBulletCount < maxLaserBulletCount) {
             var inst = Instantiate(laserBullet, transform.position, Quaternion.identity);
+            var LaserBulletScript = inst.GetComponent<LaserBullet>();
+            LaserBulletScript.cameraHeight = cameraHeight + 0.5f;  // to przechodzi z ShootingLaserFromPlanet  do LaserBullet  (0.5 aby zniknął 0.5 m poza zasięgiem wzroku kamery)
+            LaserBulletScript.cameraWidth = cameraWidth + 0.5f; // to samo tylko z szerokością
+            LaserBulletScript.ShootingLaserFromPlanet = this;
             inst.GetComponent<Rigidbody2D>().AddForce(transform.up * forcePower);
             laserBulletCount++;
+            
         }
+        
     }
 
-
-//        if (Input.GetMouseButton(0))
-//        {
-//            mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
-//            mousePosition.z = 0;
-//            distance = Vector3.Distance(transform.position, mousePosition);
-//            if (distance >= minDistance && distance <= maxDistance)
-//            {
-//                target.transform.position = mousePosition;
-//            }
-//            else if(distance > maxDistance)
-//            {
-//                if (mousePosition.x == 0)
-//                    mousePosition.x = 0.00001f;  
-//                float x = Mathf.Sqrt((maxDistance * maxDistance) / (1 + (mousePosition.y / mousePosition.x) * (mousePosition.y / mousePosition.x)));
-//                float y = x * (mousePosition.y / mousePosition.x);
-//                temp = new Vector3(x, y, 0);
-//                if (Vector3.Distance(mousePosition, temp) > Vector3.Distance(mousePosition, -temp))
-//                    target.transform.position = (-temp);
-//                else
-//                    target.transform.position = temp;
-//
-//            }
-//            else if (distance < minDistance && distance >= 0.05f)
-//            {
-//                if (mousePosition.x == 0)
-//                    mousePosition.x = 0.00001f;
-//                float x = Mathf.Sqrt((minDistance * minDistance) / (1 + (mousePosition.y / mousePosition.x) * (mousePosition.y / mousePosition.x)));
-//                float y = x * (mousePosition.y / mousePosition.x);
-//                temp = new Vector3(x, y, 0);
-//                if (Vector3.Distance(mousePosition, temp) > Vector3.Distance(mousePosition, temp * (-1)))
-//                    target.transform.position = temp * (-1);
-//                else
-//                    target.transform.position = temp;
-//            }
-//
-//        }
-//        else if (Input.GetMouseButtonUp(0))
-//        {
-//            // 1 liczenie siły na podstawie odległości (najprawdopodobniej wystarczy "kierunek")
-//            direction = transform.position - target.transform.position;
-//            // 2 spawnowanie pocisku
-//            var inst = Instantiate(pocisk, transform.position, Quaternion.identity);
-//            // 3 nadawanie siły aby pocisk se poleciał
-//            inst.GetComponent<Rigidbody2D>().AddForce(direction * 100f);
-//            // 4 reset pozycji cięciwy
-//            target.transform.position = targetStartPosition;
-//        }
-    }
+}
     
     
 
